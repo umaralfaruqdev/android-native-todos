4ec75f3529e418a4b026d7c6455df8cc322936f7
umaralfaruqdev <ualfaruq59@gmail.com>
2020-07-28 17:42:22 +0700

    init

---
+++ app/src/main/java/com/umardev/todoscli/MainActivity.java
@@ -0,0 +1,12 @@
+package com.umardev.todoscli;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle saveInstanceState) {
+		super.onCreate(saveInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
